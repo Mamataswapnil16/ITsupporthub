4521977dce5c04f71dba7d69655a109b569c3672
Mamataswapnil16 <swapnilgupta982@gmail.com>
2025-04-20 11:38:39 +1000

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,10 @@
+
+document.getElementById("loginForm")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Login placeholder triggered");
+});
+
+document.getElementById("registerForm")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Register placeholder triggered");
+});
